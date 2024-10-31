2e6a027c2ec829205d1d8234e1774e0a88e4a7a5
ilonacapo <ilo@ILONA>
2024-10-31 16:01:35 +0100

    commit attempt 2

--- Capo Ilona/game.c
+++ Capo Ilona/game.c
@@ -7,7 +7,7 @@ int main()
     Joueur *joueur1 = set_joueur(joueur1), *joueur2 = set_joueur(joueur2);
     /*joueur1 = give_cards(joueur1);
     joueur2 = give_cards(joueur2);*/
-    Carte map[3][5];
+    //Carte map[3][5];
 
     printf("Début de la partie !\n");
 
